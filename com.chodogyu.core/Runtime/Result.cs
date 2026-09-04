53928e5d11088e5c855407b2c73996eab6cf4238
ChoDoGyu <doing9498@gmail.com>
2026-09-04 21:04:07 +0900

    feat: Result 타입 추가

---
+++ com.chodogyu.core/Runtime/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CDG.Core.Results
+{
+    public sealed class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public ResultError Error { get; }
+
+        private Result(bool isSuccess, ResultError error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success()
+        {
+            return new Result(true, ResultError.None);
+        }
+
+        public static Result Failure(ResultError error)
+        {
+            if (error == null)
+            {
+                throw new ArgumentNullException(nameof(error));
+            }
+
+            if (ReferenceEquals(error, ResultError.None))
+            {
+                throw new ArgumentException("Failure result requires an error.", nameof(error));
+            }
+
+            return new Result(false, error);
+        }
+    }
+}
